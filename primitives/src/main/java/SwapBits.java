65a54836754bebf3bcc35efe4ff54eaa59ef281d
Zakaria Benjebara <zakaria.benjebara@nestle.com>
2017-09-07 12:05:21 +0100

    Solution of swap bits problem

--- primitives/src/main/java/SwapBits.java
+++ primitives/src/main/java/SwapBits.java
@@ -1,11 +1,16 @@
-public class SwapBits {
+public final class SwapBits {
 
     /*
-    5.2
+    5.2. Problem
     */
 
     public static long swapBits(long x, int i, int j) {
-
-        return 0L;
+        if (((x >>> i) & 1) != ((x >>> j) & 1)) {
+            final long MASK = (1 << i) | (1 << j);
+            x ^= MASK;
+        }
+        return x;
     }
+
+    private SwapBits() {}
 }
